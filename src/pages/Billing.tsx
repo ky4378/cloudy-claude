df3742ae5c29a591d1824e17fdebb6d070dd71a7
darius <darius@oha-global.com>
2026-09-19 06:59:46 +0800

    Reduce padding to allow billing cards to fill entire screen width

--- src/pages/Billing.tsx
+++ src/pages/Billing.tsx
@@ -253,7 +253,7 @@ export default function Billing() {
       <GlassBackdrop grid={false} />
 
       {/* Main content */}
-      <main className="px-4 pt-24 pb-16 lg:ml-[330px] lg:px-4 lg:pt-10">
+      <main className="px-4 pt-24 pb-16 lg:ml-[330px] lg:px-2 lg:pt-10">
         <div className="flex w-full flex-col gap-8">
           <div className="flex items-center gap-3">
             <div className="flex size-11 items-center justify-center rounded-2xl bg-forest-600 text-white shadow-sm">
